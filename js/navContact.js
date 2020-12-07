fabec07b3f9443f0b3eeb92e400aaca32163ca50
rea-jin <56153015+rea-jin@users.noreply.github.com>
2020-12-07 21:42:51 +0900

    フォームバリデーションまで完成

--- js/navContact.js
+++ js/navContact.js
@@ -3,20 +3,24 @@
 Vue.component('nav-contact', {
     data() { //data()でdataを定義
         return {
-            msg: 'hello contact'
+            msg: 'hello contact',
+            errors: [],
+            name: null,
+            email: null,
+            title: null
         }
     },
     template: `
     <div class="content__menu5">
         <form @submit.prevent="exec" class="contact"
         id="app"
-        @submit="checkForm"
+        @submit="checkForm" // 送信・・・・・・・２
         action="https://vuejs.org/"
         method="post"
         >
         <!-- エラー表示 -->
         <p v-if="errors.length">
-            <b>Please correct the following error(s):</b>
+            <b>エラーがあります</b>
             <ul>
             <li v-for="error in errors">{{ error }}</li>
             </ul>
@@ -25,17 +29,48 @@ Vue.component('nav-contact', {
 
             問い合わせフォーム<br>
             <label for="name">お名前</label><br>
-            <input id="name" v-model="name" type="text" name="name" placeholder="name" /><br>
+            <input id="name" v-model="name" type="text" name="name" placeholder="お名前" /><br>
             <label for="email" >メールアドレス</label><br>
-            <input id="email" v-model="email" type="email" name="email" placeholder="email" /><br>
+            <input id="email" v-model="email" type="email" name="email" placeholder="メールアドレス" /><br>
             <label for="title">件名</label><br>
-            <input id="title" type="title" placeholder="title" /><br>
+            <input id="title" type="title" placeholder="件名" /><br>
             <label for="text" >内容</label><br>
             <textarea cols="40" rows="10" maxlength="30" placeholder="まだ使えません"></textarea>
-            <button type="submit" value="submit">submit</button>
+            <button type="submit" 
+                    value="submit"
+                    @clickbtn // 送信確認・・・・・・・１
+                    >submit</button>
         </form>
 
-    </div>`
+    </div>`,
+
+    methods:{
+      checkForm: function (e) {
+        this.errors = [];
+  
+        if (!this.name) {
+          this.errors.push("Name required.");
+        }
+        if (!this.email) {
+          this.errors.push('Email required.');
+        } else if (!this.validEmail(this.email)) {
+          this.errors.push('Valid email required.');
+        }
+  
+        if (!this.errors.length) {
+          return true;
+        }
+  
+        e.preventDefault();
+      },
+      validEmail: function (email) {
+        var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+      }
+    }
+
+
+
  })
  const app = new Vue({
   el: '#app',
@@ -47,20 +82,26 @@ Vue.component('nav-contact', {
   },
   methods:{
     checkForm: function (e) {
-      if (this.name && this.age) {
-        return true;
-      }
-
       this.errors = [];
 
       if (!this.name) {
-        this.errors.push('Name required.');
+        this.errors.push("Name required.");
       }
       if (!this.email) {
-        this.errors.push('email required.');
+        this.errors.push('Email required.');
+      } else if (!this.validEmail(this.email)) {
+        this.errors.push('Valid email required.');
+      }
+
+      if (!this.errors.length) {
+        return true;
       }
 
       e.preventDefault();
+    },
+    validEmail: function (email) {
+      var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
     }
   }
 })
